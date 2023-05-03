ccb730685a7080736267ed11b8b0792c6e09f0c9
Carlos Olano <carlos.olano@live.com>
2023-05-03 19:00:17 +0200

    update5

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -68,8 +68,8 @@ const Home = () => {
 
         <p className="homeText">
           About me... just to say that I love spending time with my family, my
-          friends, playing sports, being happy as much as possible during the
-          day and most of all "The Spanish aperitivo".
+          friends, playing sports and most of all being happy as much as
+          possible during the day".
         </p>
         <button onClick={() => navigate("/projects")} className="button">
           <h2 className="buttontext">PROJECTS</h2>
